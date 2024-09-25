b0c9baa1c9e7af98f434553cb559afcb222f1201
Jason Umiker <jason.umiker@gmail.com>
2024-09-25 21:19:19 +1000

    Removing unnecessary files

--- capi-init/connect-kubectl.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-gcloud container clusters get-credentials default-capi-gke-control-plane --region=australia-southeast1
--- capi-init/install-argo-cd.sh
+++
@@ -1,3 +0,0 @@
-helm repo add argo https://argoproj.github.io/argo-helm
-helm repo update
-helm install argo-cd argo/argo-cd --create-namespace --values argo-helm-values.yaml -n argocd 
