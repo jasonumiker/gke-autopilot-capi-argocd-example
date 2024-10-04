b9fe68b211431d733f93cb2c621805663bd69ee5
Jason Umiker <jason.umiker@gmail.com>
2024-10-04 22:05:38 +1000

    Optimising Argo resource usage

--- capi-init/clusterctl-bootstrap.sh
+++ capi-init/clusterctl-bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+kubectl apply -f metrics-server.yaml
 export GCP_B64ENCODED_CREDENTIALS=$( cat ~/capi-sa.json | base64 | tr -d '\n' )
 export EXP_CAPG_GKE=true
 clusterctl init --infrastructure gcp --addon helm
